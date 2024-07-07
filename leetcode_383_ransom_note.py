236e86b528d2943dbcbf8e18f6f1bd6fc71a0b0f
codeadvance <codemoneynow@gmail.com>
2024-07-07 15:14:08 -0400

    Used Github Desktop to modify
    Added string problem

--- leetcode_383_ransom_note.py
+++ leetcode_383_ransom_note.py
@@ -1,4 +1,5 @@
 '''
+String problem
 383. Ransom Note
 Given two strings ransomNote and magazine, return true if ransomNote can be constructed by using the letters from magazine and false otherwise.
 
